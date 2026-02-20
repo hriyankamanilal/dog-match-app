15b2fc9f15e205729cddfae150523a71a0a60000
hriyankamanilal <hriyanka.manilal@gmail.com>
2026-02-20 14:57:41 -0500

    Add FavoritesProvider and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FavoritesProvider } from "@/context/FavoritesContext";
 import Index from "./pages/Index";
 import Breeds from "./pages/Breeds";
 import BreedDetail from "./pages/BreedDetail";
 import Quiz from "./pages/Quiz";
 import Results from "./pages/Results";
 import Adopt from "./pages/Adopt";
+import Favorites from "./pages/Favorites";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,15 +21,18 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/breeds" element={<Breeds />} />
-          <Route path="/breeds/:id" element={<BreedDetail />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/adopt" element={<Adopt />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <FavoritesProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/breeds" element={<Breeds />} />
+            <Route path="/breeds/:id" element={<BreedDetail />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/adopt" element={<Adopt />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </FavoritesProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
