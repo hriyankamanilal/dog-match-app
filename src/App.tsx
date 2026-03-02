393bcb49fc8fef0a43bef27f4db4978d07f6bd04
hriyankamanilal <hriyanka.manilal@gmail.com>
2026-03-02 18:21:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FavoritesProvider } from "@/context/FavoritesContext";
+import { CompareProvider } from "@/context/CompareContext";
+import ScrollToTop from "@/components/ScrollToTop";
+import CompareBar from "@/components/CompareBar";
 import Index from "./pages/Index";
 import Breeds from "./pages/Breeds";
 import BreedDetail from "./pages/BreedDetail";
@@ -11,8 +14,8 @@ import Quiz from "./pages/Quiz";
 import Results from "./pages/Results";
 import Adopt from "./pages/Adopt";
 import Favorites from "./pages/Favorites";
+import Compare from "./pages/Compare";
 import NotFound from "./pages/NotFound";
-import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -24,16 +27,20 @@ const App = () => (
       <BrowserRouter>
         <ScrollToTop />
         <FavoritesProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/breeds" element={<Breeds />} />
-            <Route path="/breeds/:id" element={<BreedDetail />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/adopt" element={<Adopt />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <CompareProvider>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/breeds" element={<Breeds />} />
+              <Route path="/breeds/:id" element={<BreedDetail />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/adopt" element={<Adopt />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/compare" element={<Compare />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <CompareBar />
+          </CompareProvider>
         </FavoritesProvider>
       </BrowserRouter>
     </TooltipProvider>
