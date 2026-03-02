41ec56ede8e6d7caae97d00b75e415eded490dd9
hriyankamanilal <hriyanka.manilal@gmail.com>
2026-03-02 18:09:48 -0500

    Add ScrollToTop component to App

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Results from "./pages/Results";
 import Adopt from "./pages/Adopt";
 import Favorites from "./pages/Favorites";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <FavoritesProvider>
           <Routes>
             <Route path="/" element={<Index />} />
